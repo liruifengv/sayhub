0587de02aae6d1580102a0e377509ea666f1c9ca
liruifengv <liruifeng1024@gmail.com>
2022-07-14 15:11:35 +0800

    feat: Home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,17 @@
 import React from 'react'
+import styles from './Home.module.css'
+import welcome from '../../static/welcome.svg'
 
-const Home = () => <div>Home</div>
+const Home = () => (
+  <div>
+    <div className={styles.welcome}>
+      <div className={styles.left}>
+        <h1>Welcome to SayHub</h1>
+        <div>Homepage of liruifeng</div>
+      </div>
+      <img src={welcome} alt="Welcome" />
+    </div>
+  </div>
+)
 
 export default Home
