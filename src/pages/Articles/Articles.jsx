9175e0036f45f9deb1786fc92ae54fe1ace58b78
liruifengv <liruifeng1024@gmail.com>
2022-07-14 15:52:00 +0800

    feat: 完善项目页面

--- src/pages/Articles/Articles.jsx
+++ src/pages/Articles/Articles.jsx
@@ -7,22 +7,26 @@ const openUrl = url => {
 }
 
 const Articles = () => (
-  <div className={styles.container}>
-    {articles.map((item, index) => (
-      <div
-        className={styles.item}
-        aria-hidden="true"
-        // eslint-disable-next-line react/no-array-index-key
-        key={index}
-        onClick={() => {
-          openUrl(item.link)
-        }}
-      >
-        <div className={styles.title}>{item.title}</div>
-        <div className={styles.time}>{item.time}</div>
-      </div>
-    ))}
-  </div>
+  <>
+    <div className={styles.pagetitle}>Articles</div>
+    <div className={styles.pagedescription}>我的文章一览</div>
+    <div className={styles.container}>
+      {articles.map((item, index) => (
+        <div
+          className={styles.item}
+          aria-hidden="true"
+          // eslint-disable-next-line react/no-array-index-key
+          key={index}
+          onClick={() => {
+            openUrl(item.link)
+          }}
+        >
+          <div className={styles.title}>{item.title}</div>
+          <div className={styles.time}>{item.time}</div>
+        </div>
+      ))}
+    </div>
+  </>
 )
 
 export default Articles
--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -1,5 +1,32 @@
 import React from 'react'
+import styles from './Projects.module.css'
+import projects from './Projects.json'
 
-const Projects = () => <div>Projects</div>
+const openUrl = url => {
+  window.open(url)
+}
+
+const Projects = () => (
+  <>
+    <div className={styles.pagetitle}>Projects</div>
+    <div className={styles.pagedescription}>我的项目一览</div>
+    <div className={styles.container}>
+      {projects.map((item, index) => (
+        <div
+          className={styles.item}
+          aria-hidden="true"
+          // eslint-disable-next-line react/no-array-index-key
+          key={index}
+          onClick={() => {
+            openUrl(item.link)
+          }}
+        >
+          <div className={styles.title}>{item.title}</div>
+          <div className={styles.description}>{item.description}</div>
+        </div>
+      ))}
+    </div>
+  </>
+)
 
 export default Projects
