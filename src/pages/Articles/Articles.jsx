bfb998b0dafd3b05eca63c7f4c2a98a6d7db81a5
liruifengv <liruifeng1024@gmail.com>
2022-07-14 15:12:18 +0800

    feat: Articles

--- src/pages/Articles/Articles.jsx
+++ src/pages/Articles/Articles.jsx
@@ -1,5 +1,28 @@
 import React from 'react'
+import styles from './Articles.module.css'
+import articles from './Articles.json'
 
-const Articles = () => <div>Articles</div>
+const openUrl = url => {
+  window.open(url)
+}
+
+const Articles = () => (
+  <div className={styles.container}>
+    {articles.map((item, index) => (
+      <div
+        className={styles.item}
+        aria-hidden="true"
+        // eslint-disable-next-line react/no-array-index-key
+        key={index}
+        onClick={() => {
+          openUrl(item.link)
+        }}
+      >
+        <div className={styles.title}>{item.title}</div>
+        <div className={styles.time}>{item.time}</div>
+      </div>
+    ))}
+  </div>
+)
 
 export default Articles
