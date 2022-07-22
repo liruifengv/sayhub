f8478f6892895c9ecd661dd5ce5e202d27473385
liruifengv <liruifeng1024@gmail.com>
2022-07-22 11:51:14 +0800

    chore: 修改 base

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,10 @@ import { BrowserRouter as Router } from 'react-router-dom'
 import routes from './router'
 
 function App() {
+  // base 仅仅是为了 github-pages，部署到自己网站时不需要，要注释掉。想想怎么自动化处理
+  const basename = process.env.NODE_ENV === 'development' ? '' : '/sayhub/'
   return (
-    <Router>
+    <Router basename={basename}>
       <Suspense>{renderRoutes(routes)}</Suspense>
     </Router>
   )
--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import eslint from 'vite-plugin-eslint'
 // https://vitejs.dev/config/
 export default defineConfig({
   // base 仅仅是为了 github-pages，部署到自己网站时不需要，要注释掉。想想怎么自动化处理
-  base: '/sayhub/', 
+  base: process.env.NODE_ENV === 'development' ? '' : '/sayhub/', 
   plugins: [react(), eslint()],
   resolve: {
     alias: {
