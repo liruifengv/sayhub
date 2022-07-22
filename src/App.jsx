118626dcfc7275292bf97d37040ba50f02752b55
liruifengv <liruifeng1024@gmail.com>
2022-07-22 14:34:08 +0800

    chore: github-ci 打包时修改环境变量

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,7 @@ import routes from './router'
 
 function App() {
   console.log('import.meta:', import.meta)
-  console.log('process.env:', process.env)
-  // base 仅仅是为了 github-pages，部署到自己网站时不需要，要注释掉。想想怎么自动化处理
-  const basename = import.meta.env ? '' : '/sayhub/'
+  const basename = import.meta.env.VITE_GITHUB_CI ? '/sayhub/' : ''
   return (
     <Router basename={basename}>
       <Suspense>{renderRoutes(routes)}</Suspense>
