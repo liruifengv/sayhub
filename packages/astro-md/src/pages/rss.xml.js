eabc5ca936f059c33c7002a110344bf42db19c37
liruifengv <liruifeng1024@gmail.com>
2022-11-24 16:27:42 +0800

    feat: astro blog init

--- packages/astro-md/src/pages/rss.xml.js
+++ packages/astro-md/src/pages/rss.xml.js
@@ -1,29 +1,10 @@
 import rss from '@astrojs/rss'
+import { SITE_TITLE, SITE_DESCRIPTION } from '../config'
 
-import { SITE, BLOG } from '~/config.mjs'
-import { fetchPosts } from '~/utils/posts'
-import { getPermalink } from '~/utils/permalinks'
-
-export const get = async () => {
-  if (BLOG.disabled) {
-    return new Response(null, {
-      status: 404,
-      statusText: 'Not found',
-    })
-  }
-
-  const posts = await fetchPosts()
-
-  return rss({
-    title: `${SITE.name}’s Blog`,
-    description: SITE.description,
+export const get = () =>
+  rss({
+    title: SITE_TITLE,
+    description: SITE_DESCRIPTION,
     site: import.meta.env.SITE,
-
-    items: posts.map((post) => ({
-      link: getPermalink(post.slug, 'post'),
-      title: post.title,
-      description: post.description,
-      publishDate: post.publishDate,
-    })),
+    items: import.meta.glob('./blog/**/*.{md,mdx}'),
   })
-}
