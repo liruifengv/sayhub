cfc369d52c790000063b2c639824097b8eb2d8c6
liruifengv <liruifeng1024@gmail.com>
2022-11-24 20:02:20 +0800

    chore: add tailwind

--- packages/astro-md/astro.config.mjs
+++ packages/astro-md/astro.config.mjs
@@ -4,12 +4,14 @@ import { defineConfig } from 'astro/config'
 import mdx from '@astrojs/mdx'
 import image from '@astrojs/image'
 import sitemap from '@astrojs/sitemap'
+import tailwind from '@astrojs/tailwind';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 // https://astro.build/config
 export default defineConfig({
   site: 'http://sayhub.me',
   integrations: [
+    tailwind(),
     mdx(),
     sitemap(),
     image({
