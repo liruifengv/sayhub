5216c516383ddc8382221a4d98a60aa408a958c3
liruifengv <liruifeng1024@gmail.com>
2022-12-14 12:48:13 +0800

    fix: cdn use https

--- packages/astro-md/src/config.ts
+++ packages/astro-md/src/config.ts
@@ -1,3 +1,3 @@
 export const SITE_TITLE = 'SayHub'
 export const SITE_DESCRIPTION = '李瑞丰的博客网站，包括个人文章、自我介绍等内容'
-export const CDN_URL = 'http://images.sayhub.me'
+export const CDN_URL = 'https://images.sayhub.me'
