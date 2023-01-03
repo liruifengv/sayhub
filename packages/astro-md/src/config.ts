a23335f4f978ca9c9c646a2081d36e04f11eedd2
liruifengv <liruifeng1024@gmail.com>
2023-01-03 20:32:38 +0800

    feat: 更新 SEO描述

--- packages/astro-md/src/config.ts
+++ packages/astro-md/src/config.ts
@@ -1,3 +1,3 @@
 export const SITE_TITLE = 'SayHub'
-export const SITE_DESCRIPTION = '李瑞丰的博客网站，包括个人文章、自我介绍等内容'
+export const SITE_DESCRIPTION = '李瑞丰/liruifengv的博客网站，包括个人文章、自我介绍等内容，技术文章涵盖前端、后端、运维、Rust、Vue、React、Node、Deno等多个领域。'
 export const CDN_URL = 'https://images.sayhub.me'
