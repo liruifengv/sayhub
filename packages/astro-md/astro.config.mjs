9699f002569688b0ed248d09b699e374fdce2211
liruifengv <liruifeng1024@gmail.com>
2023-08-07 15:21:54 +0800

    chore: 使用 vercel 图像服务

--- packages/astro-md/astro.config.mjs
+++ packages/astro-md/astro.config.mjs
@@ -28,5 +28,6 @@ export default defineConfig({
   },
   adapter: vercel({
     analytics: true,
+    imageService: true,
   }),
 });
--- packages/astro-md/src/pages/rss.xml.js
+++ packages/astro-md/src/pages/rss.xml.js
@@ -1,4 +1,4 @@
-import rss, { pagesGlobToRssItems } from '@astrojs/rss'
+import rss from '@astrojs/rss'
 import { SITE_DESCRIPTION, SITE_TITLE } from '../config'
 import { getCollection } from 'astro:content'
 
