820c3fd52bca0ec6226ec69e69f589f365b358bf
liruifengv <liruifeng1024@gmail.com>
2023-08-07 13:44:58 +0800

    chore: 修改网站说明

--- packages/astro-md/src/config.ts
+++ packages/astro-md/src/config.ts
@@ -1,5 +1,5 @@
 export const SITE_TITLE = 'SayHub'
 export const SITE_URL = 'https://sayhub.me'
 export const SITE_DESCRIPTION =
-  '使用 Astro 构建的博客模板，快速搭建拥有漂亮主题的个人博客 / A blog site template with a beautiful theme built with Astro'
+  '李瑞丰/liruifengv的博客网站，包括个人文章、自我介绍等内容，技术文章涵盖前端、后端、运维、Rust、Vue、React、Node、Deno等多个领域。'
 export const CDN_URL = 'https://images.sayhub.me'
