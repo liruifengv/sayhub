67b9230cf221a85e3c67e1ff65b48482049f9729
liruifengv <liruifeng1024@gmail.com>
2023-08-08 13:51:51 +0800

    chore: Update site title

--- packages/astro-md/src/config.ts
+++ packages/astro-md/src/config.ts
@@ -1,4 +1,4 @@
-export const SITE_TITLE = 'SayHub'
+export const SITE_TITLE = 'SayHub 李瑞丰/liruifengv的个人博客网站'
 export const SITE_URL = 'https://sayhub.me'
 export const SITE_DESCRIPTION =
   '李瑞丰/liruifengv的博客网站，包括个人文章、自我介绍等内容，技术文章涵盖前端、后端、运维、Rust、Vue、React、Node、Deno等多个领域。'
