a31a7cc425311ea381cbccfc5132d9cf1b194863
liruifengv <liruifeng1024@gmail.com>
2023-08-23 14:37:59 +0800

    fix: test twitter card

--- packages/astro-md/astro.config.mjs
+++ packages/astro-md/astro.config.mjs
@@ -10,7 +10,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url))
 // https://astro.build/config
 export default defineConfig({
   site: 'https://sayhub.me',
-  trailingSlash: 'never'
+  trailingSlash: 'never',
   experimental: {
     assets: true,
     viewTransitions: true,
